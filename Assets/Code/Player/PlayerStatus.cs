3ca44b230c85590d2f55550f82f922cf35e56744
AnttilaJoni <jonianttila110@gmail.com>
2026-03-31 09:52:46 +0300

    Pelaaja ottaa damagea

--- Assets/Code/Player/PlayerStatus.cs
+++ Assets/Code/Player/PlayerStatus.cs
@@ -12,24 +12,32 @@ public class PlayerStatus : MonoBehaviour
     public bool hasKey_4;
 
     //Max arvot healthille, heal itemeille, ammolle.
-    public int playerMaxHealth;
-    public int maxHealthItems;
-    public int maxPlayerAmmo;
+    public int playerMaxHealth = 6;
+    public int maxHealthItems = 15;
+    public int maxPlayerAmmo = 300;
 
     //Potionin healaama määrä, healaus
-    public int potionHealAmount;
+    public int potionHealAmount = 3;
 
     private bool healing;
     //private float healTime;
     //private float healCooldown;
 
+    //Tämänhetkinen health, heal item määrä, ammo määrä.
+    public int playerHealth = 6;
+    public int healthItemPool = 5;
+    public int ammoPool = 25;
+
     private PlayerInput _playerInput;
     InputAction heal;
 
-    //Tämänhetkinen health, heal item määrä, ammo määrä.
-    public int playerHealth;
-    public int healthItemPool;
-    public int ammoPool;
+    //public float cooldown = 8f;
+
+    public AudioClip[] PlayerAudioClips;
+    [Range(0, 1)] public float PlayerAudioVolume = 0.5f;
+
+    public bool hitOnce = false;
+    public bool hitTwice = false;
 
     private void Start()
     {
@@ -44,7 +52,68 @@ public class PlayerStatus : MonoBehaviour
         {
             AddHealth();
         } 
-           
+
+        /* if(playerHealth == (playerMaxHealth / 3) * 2 )
+        {
+            if(hitOnce == false) 
+            {
+                OnHit();
+                hitOnce = true;
+            }
+        }
+        
+        if(playerHealth == playerMaxHealth / 3)
+        {
+            if(hitTwice == false) 
+            {
+                OnHit();
+                hitTwice = true;
+            }
+        } */
+
+        /* if(playerHealth <= 0)
+        {
+            OnHit();
+            //gameObject.SetActive(false);
+            //Invoke("ActivatePlayer", cooldown);
+        } */
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<MonsterHealth>() != null)
+        {
+            if(playerHealth >= 1)
+            {
+                OnHit();
+                playerHealth -= 1;
+            }
+            /* if(playerHealth == 1)
+            {
+                OnHit();
+                playerHealth -= 1;
+            } */
+
+        }
+            
+
+    }
+
+    private void OnHit()
+    {
+        if (PlayerAudioClips.Length > 0)
+            {
+                var index = Random.Range(0, PlayerAudioClips.Length);
+                AudioSource.PlayClipAtPoint(PlayerAudioClips[index], transform.TransformPoint(this.transform.position), PlayerAudioVolume);
+            }
+    }
+
+    void ActivatePlayer()
+    {
+        gameObject.SetActive(true);
+        playerHealth = playerMaxHealth;
+        //hitOnce = false;
+        //hitTwice = false;
     }
     
     public void AddHealth()
