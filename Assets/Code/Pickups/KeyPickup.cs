9a5a3897aee7c5b111f2aa5ef2033e424baae505
AnttilaJoni <jonianttila110@gmail.com>
2026-04-01 11:50:43 +0300

    Save system melkein valmis.

--- Assets/Code/Pickups/KeyPickup.cs
+++ Assets/Code/Pickups/KeyPickup.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
 
 public class KeyPickup : MonoBehaviour
+//, IDataPersistence
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /* [SerializeField] private string id;
+
+    [ContextMenu("Generate guid for id")]
+    private void GenerateGuid()
     {
-        
+        id = System.Guid.NewGuid().ToString();
     }
 
-    // Update is called once per frame
+    public int ammoPickupAmount = 15;
+    private bool collected = false;
+
+    public void LoadData(GameData data)
+    {
+        data.ammoCollected.TryGetValue(id, out collected);
+        if (collected)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        if (data.ammoCollected.ContainsKey(id))
+        {
+            data.ammoCollected.Remove(id);
+        }
+        data.ammoCollected.Add(id, collected);
+    }
+
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0f, 100f, 0f) * Time.deltaTime);
     }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(!collected)
+        {
+            Weapon weapon = collision.gameObject.GetComponentInChildren<Weapon>();
+            if (weapon)
+            {
+                weapon.AddAmmo(ammoPickupAmount);
+                collected = true;
+                Destroy(gameObject);
+            }
+        }
+    } */
 }
