4841322958e089b8e63bf6847d1deaedfdd7fa29
AnttilaJoni <jonianttila110@gmail.com>
2026-04-08 14:06:38 +0300

    toimii

--- Assets/Code/Cutscene/CutsceneHandler.cs
+++ Assets/Code/Cutscene/CutsceneHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CutsceneHandler : MonoBehaviour
 {
@@ -14,8 +15,23 @@ public class CutsceneHandler : MonoBehaviour
 
     private void ExecuteCurrentElement()
     {
+        Scene scene = SceneManager.GetActiveScene();
         if(index >= 0 && index < cutsceneElements.Length)
+        {
             cutsceneElements[index].Execute();
+        } 
+        else if(scene.name == $"Main Menu")
+        {   
+            DataPersistenceManager.instance.NewGame();
+            SceneManager.LoadSceneAsync(1);
+            
+            Cursor.lockState = CursorLockMode.Locked;
+            Time.timeScale = 1;
+            AudioListener.pause = false;
+        }
+            
+            
+        
     }
 
     public void PlayNextElement()
