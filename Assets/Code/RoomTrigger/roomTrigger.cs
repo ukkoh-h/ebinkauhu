68ab099251e509f2a12dd008a61990a44a4d73f2
ukkoh-h <ukkohh@gmail.com>
2026-04-08 13:09:58 +0300

    musiikki systeemit

--- Assets/Code/RoomTrigger/roomTrigger.cs
+++ Assets/Code/RoomTrigger/roomTrigger.cs
@@ -15,6 +15,7 @@ public class roomTrigger : MonoBehaviour
     public lockedStatus door3;
     public bool visibility;
     public bool desapawnMonster;
+    public bool safeRoom;
     public bool respawnMonster;
     public bool spawnBehindWall;
     public bool finalScene;
@@ -26,6 +27,7 @@ public class roomTrigger : MonoBehaviour
             if (!visibility) monster2.cantSee = true;
             if (desapawnMonster) 
             {
+                if (safeRoom)monster2.MusicSafeRoom();
                 monster1.SetActive(false);
             }
             if (spawnBehindWall)
